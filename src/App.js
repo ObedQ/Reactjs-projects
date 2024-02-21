02b7d9c1d02fcbecd0b584680e3c372eb8fd8dfc
ObedQ <olotu@st.ug.edu.gh>
2024-02-21 09:56:54 +0000

    Random Color completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
      {/*Accordian component*/ }
-     <Accordian/>
+     {/* <Accordian/> */}
      {/*Random Color component*/ }
       <RandomColor/>
     </div>
