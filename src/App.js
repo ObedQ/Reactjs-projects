d63b5c73cb5e662312dab3ba533d11335da72aae
ObedQ <olotu@st.ug.edu.gh>
2024-02-21 13:43:53 +0000

    image slider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-     {/*Accordian component*/ }
-     {/* <Accordian/> */}
-     {/*Random Color component*/ }
+      {/*Accordian component*/}
+      {/* <Accordian/> */}
+      {/*Random Color component*/}
       {/* <RandomColor/> */}
       {/* star rating */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* image-slider */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
